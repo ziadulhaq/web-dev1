24b51fd318cc17b2f91d2d570d0df53b5451a3fa
MD.ZIADUL HAQ JOY <116157050+ziadulhaq@users.noreply.github.com>
2022-11-14 17:24:45 +0600

    Add files via upload

---
+++ PHP/teast.php
@@ -0,0 +1,47 @@
+<?php
+ $name01=55;
+ $name02=38;
+ $result=$name01+$name02;
+?>
+ welcome
+<h1>Ziadul</h1>
+<?php
+echo 'this is my first project in php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="one">
+        <h1>ziadul</h1>
+    </div>
+    <div class="site">abc</div>
+    
+
+ <div class="tree">
+ <?php
+echo 'the muliplication=' .$result;
+if ($name01===$name02){
+    echo '<br> true';
+}
+else{
+    echo '<br>false';
+}
+var_dump($name01);
+print_r($result);
+
+
+    ?>
+    <?php
+    $t = date()
+    ?>
+ </div>
+</body>
+</html>
